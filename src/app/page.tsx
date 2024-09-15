68626d85c090e4305302f79e61f81acbbb80477a
Roshan Kumar Singh <156603960+RKS1999@users.noreply.github.com>
2024-09-15 14:35:02 +0530

    Banner Updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import Image from "next/image";
+"use client";
 import ProductList from "./products/page";
 import "./globals.css";
 import Contact from "@/app/contact/page";
@@ -14,12 +14,10 @@ const Home: React.FC = () => {
   return (
     <>
       <div style={bannerWrapperStyle}>
-        <Image
+        <img
           src="https://img.freepik.com/free-psd/horizontal-banner-online-fashion-sale_23-2148585404.jpg?w=1380&t=st=1726322983~exp=1726323583~hmac=6567ea89ec00f1c23be07a047a47431c45bd716cff76bc63a8ea664c7b469d61"
           alt="Banner"
-          layout="fill"
-          objectFit="cover"
-          style={{ display: "block" }}
+          style={{ display: "block", width: "100%", height: "100%" }}
         />
       </div>
       <ProductList />
